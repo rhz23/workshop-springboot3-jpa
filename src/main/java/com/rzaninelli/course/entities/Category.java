5dff614915c47bb84c5ac533a191c7ce9a241ba2
rhz23 <rhzaninelli@gmail.com>
2022-11-23 19:54:11 -0300

    Corrigindo ManyToMany association nas classes Category e Product

--- src/main/java/com/rzaninelli/course/entities/Category.java
+++ src/main/java/com/rzaninelli/course/entities/Category.java
@@ -1,5 +1,6 @@
 package com.rzaninelli.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -16,7 +17,8 @@ public class Category implements Serializable {
 
     private String name;
 
-    @ManyToMany
+    @ManyToMany(mappedBy = "categories")
+    @JsonIgnore
     private Set<Product> products = new HashSet<>();
 
     public Category() {
