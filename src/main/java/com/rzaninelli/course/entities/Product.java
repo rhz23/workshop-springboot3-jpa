f17fcd9ada8b1acf27ffd7ba854e1ec65e5de6da
rhz23 <rhzaninelli@gmail.com>
2022-11-24 18:40:46 -0300

    Algumas correções no relacionamento Product e OrderItem e implementação de classe Payment

--- src/main/java/com/rzaninelli/course/entities/Product.java
+++ src/main/java/com/rzaninelli/course/entities/Product.java
@@ -1,10 +1,10 @@
 package com.rzaninelli.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
@@ -29,6 +29,9 @@ public class Product implements Serializable {
             inverseJoinColumns = @JoinColumn(name = "category_id")
     )
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product() {
     }
@@ -83,6 +86,15 @@ public class Product implements Serializable {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders(){
+        Set<Order> set = new HashSet<>();
+        for (OrderItem orderItem : items) {
+            set.add(orderItem.getOrder());
+        }
+        return set;
     }
 
     @Override
