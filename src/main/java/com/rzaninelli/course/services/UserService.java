9ab971bfeb0556da0ad3cf413b6c30ae1c015316
rhz23 <rhzaninelli@gmail.com>
2022-11-29 15:30:02 -0300

    Implementado tratamento de exceções personalizadas para requisições delete

--- src/main/java/com/rzaninelli/course/services/UserService.java
+++ src/main/java/com/rzaninelli/course/services/UserService.java
@@ -1,9 +1,12 @@
 package com.rzaninelli.course.services;
 
+import com.rzaninelli.course.controllers.exceptions.DatabaseException;
 import com.rzaninelli.course.entities.User;
 import com.rzaninelli.course.repositories.UserRepository;
 import com.rzaninelli.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,13 @@ public class UserService {
     }
 
     public void delete(Long id){
-        userRepository.deleteById(id);
+        try{
+            userRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj){
